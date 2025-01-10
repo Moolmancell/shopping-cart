0c8f594652546da6f485c397b2bb2a9edca6be7b
Moolmancell <richmondclaudebodot@gmail.com>
2025-01-10 13:15:26 +0800

    Update imports in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from './App.jsx'
+import App from './App/App.jsx'
 
 const router = createBrowserRouter([
   {
