0cc7f68b92de0533452596c71915576660b4f6a7
Moolmancell <richmondclaudebodot@gmail.com>
2025-01-14 16:18:46 +0800

    Add Shop.jsx content and tests

--- src/routes/Shop/Shop.jsx
+++ src/routes/Shop/Shop.jsx
@@ -1,11 +1,56 @@
-import { ProductCard } from "../../Components/Product Card/ProductCard"
+import { ProductCard } from "../../Components/Product Card/ProductCard";
+import { useEffect, useState } from "react";
 
 export function Shop() {
-    return <main>
-        <h1>Shop</h1>
-        <h3>Todays Best Deals for you!</h3>
-        <section>
-            <ProductCard name="Product Name" price="$1000"/>
-        </section>
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState("loading"); 
+  const [reload, setReload] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://fakestoreapi.com/products/category/electronics"
+        );
+        const products = await response.json();
+
+        setProducts(products); 
+        setLoading("success"); 
+      } catch (error) {
+        console.error("Failed to fetch products:", error);
+        setLoading("error"); 
+      }
+    };
+
+    fetchData();
+  }, [reload]); 
+
+  return (
+    <main>
+      {loading === "loading" && <h1>Loading...</h1>}
+
+      {loading === "error" && (
+        <>
+          <h1>Loading error</h1>
+          <button onClick={() => setReload((prev) => !prev)}>Try Again</button>
+        </>
+      )}
+
+      {loading === "success" && (
+        <>
+          <h1>Shop</h1>
+          <h3>Today's Best Deals for you!</h3>
+          <section>
+            {products.map((product) => (
+              <ProductCard
+                key={product.id} 
+                name={product.title}
+                price={product.price}
+              />
+            ))}
+          </section>
+        </>
+      )}
     </main>
+  );
 }
