d8f23329da486d5d04649906db1cdd99186bc8aa
Moolmancell <richmondclaudebodot@gmail.com>
2025-01-14 11:36:58 +0800

    Add addToCart states

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -4,6 +4,11 @@ import { Footer } from '../Components/Footer/Footer.jsx'
 import { Outlet } from "react-router-dom";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart((prevCart) => [...prevCart, product]);
+  };
   return <div>
     <Header/>
     <Outlet/>
