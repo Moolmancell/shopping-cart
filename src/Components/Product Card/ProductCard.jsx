453203a574f5aa26eac26cca5d9fbeee72d63d20
Moolmancell <richmondclaudebodot@gmail.com>
2025-01-14 16:18:33 +0800

    Add dollar sign

--- src/Components/Product Card/ProductCard.jsx
+++ src/Components/Product Card/ProductCard.jsx
@@ -5,7 +5,7 @@ export function ProductCard({name, price, img="", img_alt=""}) {
         <img src={img} alt={img_alt} />
         <div>
             <h2>{name}</h2>
-            <p>{price}</p>
+            <p>${price}</p>
             <div>
                 <Button>Add to cart</Button>
             </div>
