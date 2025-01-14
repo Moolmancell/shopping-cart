6092141386f91e27403301147f590f20433f912e
Moolmancell <richmondclaudebodot@gmail.com>
2025-01-14 11:01:16 +0800

    Add footer contents and its tests

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,25 @@
+import logo from '../../assets/Logo.svg';
+import { Link } from 'react-router-dom';
+
 export function Footer() {
     return <footer data-testid="footer-component">
+        <section>
+            <img src={logo} alt="" />
+            <a href="#">Contact Us</a>
+        </section>
 
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="shop">Shop</Link></li>
+                <li><Link to="about">About</Link></li>
+                <li><a href="#">Privacy Notice</a></li>
+                <li><a href="#">Terms and Conditions</a></li>
+            </ul>
+        </nav>
+
+        <section>
+            <p>© 2025 Richmond Claude S. Bodot</p>
+        </section>
     </footer>
 }
--- src/Components/Footer/Footer.test.jsx
+++ src/Components/Footer/Footer.test.jsx
@@ -1,10 +1,15 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import {Footer} from './Footer'
+import { MemoryRouter } from "react-router-dom";
 
 describe("Footer", () => {
     it("Footer rendered?", () => {
-        render(<Footer/>)
+        render(
+        <MemoryRouter>
+            <Footer/>
+        </MemoryRouter>
+        )
         expect(screen.getByTestId('footer-component')).toBeInTheDocument();
     });
 });
