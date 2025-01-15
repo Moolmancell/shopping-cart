ba4508d5d0d5706eec43a2c85383cb764eb1045e
Moolmancell <richmondclaudebodot@gmail.com>
2025-01-15 15:03:35 +0800

    Add additional props to CartItem

--- src/Components/Cart Item/CartItem.jsx
+++ src/Components/Cart Item/CartItem.jsx
@@ -2,13 +2,16 @@ import { Button } from "../Button/Button"
 
 export function CartItem(props) {
     return <div>
-        <h1>{props.name}</h1>
+        <img src={props.image} alt=""/>
+        <h3>{props.title}</h3>
         <p>${props.price}</p>
+        <p>Quantity: {props.quantity}</p>
+        <p>Total price: {props.price * props.quantity}</p>
         <div>
-            <Button>-</Button>
+            <Button onClick={props.minusQuantity}>-</Button>
             <p>{props.quantity}</p>
-            <Button>+</Button>
+            <Button onClick={props.addQuantity}>+</Button>
         </div>
-        
+        <Button onClick={props.removeFromCart}>Remove</Button>
     </div>
 }
