2f4cfe8a1b58e65fb24a4acd6436627e1d029691
Moolmancell <richmondclaudebodot@gmail.com>
2025-01-15 13:51:09 +0800

    Add $ sign

--- src/Components/Cart Item/CartItem.jsx
+++ src/Components/Cart Item/CartItem.jsx
@@ -3,7 +3,7 @@ import { Button } from "../Button/Button"
 export function CartItem(props) {
     return <div>
         <h1>{props.name}</h1>
-        <p>{props.price}</p>
+        <p>${props.price}</p>
         <div>
             <Button>-</Button>
             <p>{props.quantity}</p>
