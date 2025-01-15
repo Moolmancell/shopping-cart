cf7ab8e75237167ebde29d24d34ecc35bb6654b4
Moolmancell <richmondclaudebodot@gmail.com>
2025-01-15 23:01:50 +0800

    Remove extra li

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -19,7 +19,6 @@ export function Header() {
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="shop">Shop</Link></li>
                     <li><Link to="about">About</Link></li>
-                    <li></li>
                 </ul>
             </nav>
             {showCart ? (
