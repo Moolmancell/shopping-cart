c966ae27d802bb42129d8198a40830c4ad15fb75
Moolmancell <richmondclaudebodot@gmail.com>
2025-01-15 22:47:55 +0800

    Add ul and li to links

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
 import { Button } from "../Button/Button";
 
-export function Sidebar(setShowSidebar) {
+export function Sidebar({setShowSidebar}) {
     return (
         <div>
-            <Link to="/" onClick={() => setShowSidebar()}>Home</Link>
-            <Link to="shop" onClick={() => setShowSidebar()}>Shop</Link>
-            <Link to="about"> onClick={() => setShowSidebar()}About</Link>
             <Button onClick={() => setShowSidebar()}>x</Button>
+            <ul>
+                <li><Link to="/" onClick={() => setShowSidebar()}>Home</Link></li>
+                <li><Link to="shop" onClick={() => setShowSidebar()}>Shop</Link></li>
+                <li><Link to="about" onClick={() => setShowSidebar()}>About</Link></li>
+            </ul>
         </div>
     );
 }
